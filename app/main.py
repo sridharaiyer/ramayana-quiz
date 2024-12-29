9361505dc3d95486c783487ac7263052675e3ef9
Sridhar Iyer <iyer.a.sri@gmail.com>
2024-12-29 15:30:12 -0600

    comment for the quiz app

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+# My quizes app
 app = FastAPI()
 
 # Constants for paths
